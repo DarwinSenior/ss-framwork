ab382b4ce2b615527684177b47d28b21d673f95e
Kevin Renner <renke@wowway.com>
2014-11-10 19:49:52 -0600

    Added function to calculate scores of of songs,   and order songs by tags

--- framework/GetSongsFromTags.py
+++ framework/GetSongsFromTags.py
@@ -3,12 +3,13 @@ __author__ = 'Kevin Renner & HKJ'
 import re
 import operator
 import os
+from collections import OrderedDict
 #import songtags.dbinteract
 
 
 #Gets tag data from a CSV file
 #Returns dictionary with song names as keys and tags as values
-def getData():
+def get_data():
     data = dict()
     pwd = os.path.dirname(__file__)  # get current directory
     datafile = open(os.path.join(pwd, "data"))
@@ -22,9 +23,10 @@ def getData():
 
 
 #def getDBData():
- #   return songtags.dbinteract.get_songs_and_tags_dict()
+#    return songtags.dbinteract.get_songs_and_tags_dict()
 
-def getScoreData():
+#Returns dict containing the scores for tag comparisons
+def get_score_data():
     scoreData = dict()
     pwd = os.path.dirname(__file__)
     file = open(os.path.join(pwd, "tagscore.csv"))
@@ -34,46 +36,62 @@ def getScoreData():
     for l in lines:
         comparingTag = l.strip().split(",")[0]
         comparingTags.append(comparingTag)
-    i = 0 #Incrementing value for following for loop
+    i = 0
     for databaseTag in databaseTags:
         j = 0
         for comparingTag in comparingTags:
-            scoreData[(databaseTag, comparingTag)] = int(lines[i].strip().split(",")[j])
+            if lines[i].strip().split(",")[j].isdigit():
+                scoreData[(databaseTag, comparingTag)] = int(lines[i].strip().split(",")[j])
+            else:
+                scoreData[(databaseTag, comparingTag)] = 0
             j += 1
         i += 1
     return scoreData
 
+#Returns the result of a tag comparison
+def compare_tags(databaseTag, comparingTag):
+    scoreData = get_score_data()
+    score = scoreData[(databaseTag, comparingTag)]
+    return score
 
 #Searches for the songs that correspond to a tag
-#Returns a dictionary with song names as keys and the number of tags the song matches as the values
-def getSongs(tags):
-    data = getData() # You can also use getDBData() now to pull from the database
+#Returns an OrderedDict with song names as keys and tag match scores as the values
+def get_songs(tags):
+    data = get_data() # You can also use getDBData() now to pull from the database
     songList = dict()
-    for tag in tags:
+    for comparingTag in tags:
         for name in data:
-            compareToTag(songList, data, name, tag)
+            line = data[name]
+            score = 0
+            for databaseTag in line:
+                score += compare_tags(databaseTag, comparingTag)
+            if name in songList.keys():
+                songList[name] += score
+            else:
+                songList[name] = score
+            #if tag in line and name in songList.keys():
+            #    songList[name] += 1
+            #else:
+            #    songList[name] = 1
+    newSongList = sort_songs(songList)
+    return newSongList
 
-    return songList
+# input: songList dict, containing name keys and score values
+# returns: songList OrderedDict, sorted by score order
+def sort_songs(songList):
+    newSongList = OrderedDict(sorted(songList.iteritems(), key=operator.itemgetter(1), reverse=True))
+    return newSongList
 
-#Adds songs to the songList dict if they match a tag. Each song is initialized with a value of one, and is incremented by one for each tag matched
-def compareToTag(songList, data, name, tag):
-    line = data[name]
-    if tag in line:
-        if name in songList.keys():
-            songList[name] += 1
-        else:
-            songList[name] = 1
-
+def thesaurus(tag):
 
 
 #Runs the tag search using an array 'tags' of tags
 if __name__=="__main__":
-    #print "Welcome to song search via tag\n"
-    #print "Enter tags seperated by a comma\n"
-    #rInput = raw_input()
-    #pattern = re.compile("^\s+|\s*,\s*|\s+$")
-    #tags = [x.capitalize() for x in pattern.split(rInput) if x]
-    #for w in sorted(getSongs(tags), key=getSongs(tags).get, reverse=True):
-    #    print w, getSongs(tags)[w]
-    scoreData = getScoreData()
-    print scoreData[("Leonard Berstein", 'Orchestra')]
+    # print "Welcome to song search via tag\n"
+    # print "Enter tags seperated by a comma\n"
+    # rInput = raw_input()
+    # pattern = re.compile("^\s+|\s*,\s*|\s+$")
+    # tags = [x.capitalize() for x in pattern.split(rInput) if x]
+    # for w in sorted(get_songs(tags), key=get_songs(tags).get, reverse=True):
+    #     print w, get_songs(tags)[w]
+    print get_songs(["Loud", "Classical"])
