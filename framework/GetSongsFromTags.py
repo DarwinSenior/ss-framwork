bfb316f8e1e8af45bfc139835518fd0659b406a3
Kevin Renner <renke@wowway.com>
2014-11-10 21:04:26 -0600

    Added thesaurus api code

--- framework/GetSongsFromTags.py
+++ framework/GetSongsFromTags.py
@@ -3,6 +3,7 @@ __author__ = 'Kevin Renner & HKJ'
 import re
 import operator
 import os
+from pyhugeconnector import pyhugeconnector
 from collections import OrderedDict
 #import songtags.dbinteract
 
@@ -69,10 +70,6 @@ def get_songs(tags):
                 songList[name] += score
             else:
                 songList[name] = score
-            #if tag in line and name in songList.keys():
-            #    songList[name] += 1
-            #else:
-            #    songList[name] = 1
     newSongList = sort_songs(songList)
     return newSongList
 
@@ -83,15 +80,16 @@ def sort_songs(songList):
     return newSongList
 
 def thesaurus(tag):
+    word = pyhugeconnector.thesaurus_entry(tag, '5840e9e964f507b017c4adee93c40189', 'adjective', relationship_type = "syn")
+    return word
 
 
 #Runs the tag search using an array 'tags' of tags
 if __name__=="__main__":
-    # print "Welcome to song search via tag\n"
-    # print "Enter tags seperated by a comma\n"
-    # rInput = raw_input()
-    # pattern = re.compile("^\s+|\s*,\s*|\s+$")
-    # tags = [x.capitalize() for x in pattern.split(rInput) if x]
-    # for w in sorted(get_songs(tags), key=get_songs(tags).get, reverse=True):
-    #     print w, get_songs(tags)[w]
-    print get_songs(["Loud", "Classical"])
+    print "Welcome to song search via tag\n"
+    print "Enter tags seperated by a comma\n"
+    rInput = raw_input()
+    pattern = re.compile("^\s+|\s*,\s*|\s+$")
+    tags = [x.capitalize() for x in pattern.split(rInput) if x]
+    for w in sorted(get_songs(tags), key=get_songs(tags).get, reverse=True):
+        print w, get_songs(tags)[w]
